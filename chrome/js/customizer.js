6a6795400c979963c1afbb31c3df00d83e8a312e
Özcan Zafer AYAN <ozcanzaferayan@gmail.com>
2015-06-22 01:54:01 +0300

    Source page link added

--- chrome/js/customizer.js
+++ chrome/js/customizer.js
@@ -6,4 +6,5 @@ $(document).ready(function(){
 function setHeader(){
 	$('a#homepage').attr('href', manifest.homepage_url);
 	$('#spanTitle, .spanTitle').text(manifest.browser_action.default_title);
+	$('a#sourcepage').attr('href', manifest.permissions[2]);
 }
